893b55983222608f73b2352a9771f705da25e87b
desupink <geefuller5@gmail.com>
2018-12-24 11:37:50 +0200

    Final deployment without styling fix of active routerlink

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  user$: Object;
+
+  constructor(
+    private data: DataService,
+    private route: ActivatedRoute
+    ) {
+      this.route.params.subscribe( 
+        params => this.user$ = params.id
+      )
+     }
 
   ngOnInit() {
+
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data
+    )
   }
 
 }
